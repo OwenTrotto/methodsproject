a0de85eea21dfde894c64a72705ed7cc442b6973
OwenTrotto <47450694+OwenTrotto@users.noreply.github.com>
2019-03-07 08:49:52 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,87 @@
+package org.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        printGreetings("Owen");
+        System.out.println("Hello Owen");
+
+
+
+        int a = 3;
+        int b = 4;
+        System.out.println(multiply(a,b));
+
+
+
+        printGreetings("Hello");
+        System.out.println("Hello World");
+    }
+
+        public static void printGreetings(String noun) {
+        System.out.println("Hello "+ noun);
+
+        }
+
+
+
+
+        public static int multiply(int firstNumber, int secondNumber){
+
+        int answer = firstNumber*secondNumber;
+
+        return answer;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
